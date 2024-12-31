fc00d10cd19769e30d44f25b937c925ec4af98b3
Ulises Molina <uli.molina10@gmail.com>
2024-12-31 18:35:00 -0300

    fix bug eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname, // Asegúrate de que la ruta sea correcta
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"), // Extiende la configuración de Next.js
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    parserOptions: {
+      ecmaVersion: 2020, // Configuración adicional del parser
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true, // Soporte para JSX
+      },
+    },
+  },
+];
 
 export default eslintConfig;
